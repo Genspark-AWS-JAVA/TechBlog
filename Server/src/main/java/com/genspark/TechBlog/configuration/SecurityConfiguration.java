3ac7df7098c840c14823d92cf9dd8ceb60bf09e6
Michael Guirguis <guirguis@mguirguis.com>
2022-06-15 21:17:44 -0400

    formatting

--- Server/src/main/java/com/genspark/TechBlog/configuration/SecurityConfiguration.java
+++ Server/src/main/java/com/genspark/TechBlog/configuration/SecurityConfiguration.java
@@ -3,12 +3,9 @@ package com.genspark.TechBlog.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -23,7 +20,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                                .antMatchers("/**").permitAll()
+                .antMatchers("/**").permitAll()
 //                .antMatchers(HttpMethod.GET, "/", "/**").permitAll()
 //                .antMatchers(HttpMethod.POST, "/articles").hasAuthority(Authorities.WRITE_ARTICLE.toString())
 //                .antMatchers(HttpMethod.PUT, "/articles/**").hasAuthority(Authorities.WRITE_ARTICLE.toString())
@@ -55,7 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         Authorities.DELETE_USER.toString());
     }
 
-//    @Override
+    //    @Override
 //    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
 ////        auth.jdbcAuthentication()
 ////                .dataSource(dataSource)
