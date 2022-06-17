bf1b1375e160d99402e368a8f6e58f450e8687f9
Michael Guirguis <guirguis@mguirguis.com>
2022-06-17 00:26:20 -0400

    Update TechBlogApplication.java

--- Server/src/main/java/com/genspark/TechBlog/TechBlogApplication.java
+++ Server/src/main/java/com/genspark/TechBlog/TechBlogApplication.java
@@ -2,9 +2,6 @@ package com.genspark.TechBlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * a Tech Blog web service application
