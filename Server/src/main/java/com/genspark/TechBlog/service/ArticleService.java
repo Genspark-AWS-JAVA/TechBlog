ff10ca076dcf290904bb226019f18b2f7638f05f
Michael Guirguis <guirguis@mguirguis.com>
2022-06-20 19:22:34 -0400

    fixed search on in content

--- Server/src/main/java/com/genspark/TechBlog/service/ArticleService.java
+++ Server/src/main/java/com/genspark/TechBlog/service/ArticleService.java
@@ -9,9 +9,9 @@ public interface ArticleService extends JpaRepository<Article, Long> {
 
     Iterable<Article> findAllByUsername(String username);
 
-    Iterable<Article> findByContentOrTitleContains(String keyword1, String keyword2);
-
     Article findFirstById(long article_id);
 
     Article findFirstAllById(long id);
+
+    Iterable<Article> findByContentContainsOrTitleContains(String keyword, String keyword1);
 }
